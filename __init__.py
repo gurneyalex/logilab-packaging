9d1afb16869d72b0d6a1bf20edb67c43353cd579
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2006-08-03 18:29:44 +0200

    removed __init__.py's content

--- __init__.py
+++ __init__.py
@@ -3,11 +3,3 @@ http://www.logilab.fr/ -- mailto:contact@logilab.fr
 
 Logilab's developpment tools
 """
-
-__revision__ = '$Id: __init__.py,v 1.7 2003-09-09 12:18:34 syt Exp $'
-
-from os.path import join
-TEMPLATE_DIR = join(__path__[0], 'templates')
-del join
-
-
