434901d4f3e7834885ca846e6c968c56e7f430a3
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2006-08-03 16:22:41 +0200

    updated command management

--- lgp.py
+++ lgp.py
@@ -3,18 +3,20 @@
 import sys
 from optparse import OptionParser as OP
 
-USAGE = """lgp - logilab packaging tool
+## class Command:
 
-lgp prepare - process to prepare distrib (pylint, checkpkg, check vcs, etc.)
-lgp make - process to build distrib (build, tag, etc.)
-lgp test - test distrib (linda, lintian, piuparts)
+##     def __init__(self, module=None, run=None, add_opt=None):
+##         self.module = module
+##         self.run = run
+##         self.add_options = add_opt
 
-lgp tag - tag package
-lgp build - build debian package
-lgp check - check package 
-lgp info - gives package information
+##     def get_(self):
+##         if self.module:
+##             exec('from %s import run, add_options'%self.module)
+##             return run, add_options:
+##         else:
+##             return self.run, self.add_options
 
-"""
 
 class OptionParser(OP):
 
@@ -32,32 +34,46 @@ class OptionParser(OP):
         cmd = args[0]
 	args = args[1:]
         if cmd not in self._commands:
+            if cmd in ('-h', '--help'):
+                self.print_help()
+                print '\ncommands:'
+                for cmdname, (_, help) in self._commands.items():
+                    print '% 10s - %s' % (cmdname, help)
+                sys.exit(0)
             self.error('unknow command')
         self.prog = '%s %s' % (self.prog, cmd)
-        exec('from %s import run, add_options'%self._commands[cmd][0])
+        module, help = self._commands[cmd]
+        # optparse inserts self.description between usage and options help
+        self.description = help
+        exec('from %s import run, add_options'%module)
         add_options(self)
         (options, args) = self.parse_args(args)        
         if not (self.min_args <= len(args) <= self.max_args):
             self.error('incorrect number of arguments')
         return run, options, args
-        
+
 
 def run(args):
     parser = OptionParser()
     parser.usage = 'lgp COMMAND [options] <arg> ...'
-    COMMANDS = [('prepare','logilab.devtools.preparedist'),
-                ('make','logilab.devtools.makedist'),
-                ('test','logilab.devtools.testdist'),
-                ('build','logilab.devtools.buildpackage'),
-                ('tag','logilab.devtools.tagpackage'),
-                ('check', 'logilab.devtools.checkpackage'),
-                ('info', 'logilab.devtools.pkginfo'),
+    COMMANDS = [('prepare', 'logilab.devtools.preparedist',
+                 'process to prepare distrib (pylint, checkpkg, check vcs, etc.)'),
+                ('make', 'logilab.devtools.makedist',
+                 'process to build distrib (build, linda, lintian, piuparts, tag)'),
+                ('build', 'logilab.devtools.buildpackage',
+                 'build debian and source packages'),
+                ('tag', 'logilab.devtools.tagpackage',
+                 'tag package repository'),
+                ('check', 'logilab.devtools.checkpackage',
+                 'check that package is ready to be built'),
+                ('info', 'logilab.devtools.pkginfo',
+                 'extract info from __pkginfo__'),
                 ]
     for item in COMMANDS:
         parser.add_command(*item)
     run, options, args = parser.parse_command(sys.argv[1:])
-    run(options, args)
+    return run(options, args)
 
 if __name__ == '__main__':
-    run(sys.argv[1:])
+    sys.exit(run(sys.argv[1:]))
 
