888b569ac35f212d2941fdc9f7609aeea0870373
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2006-08-03 16:21:54 +0200

    made lgp prepare work

--- preparedist.py
+++ preparedist.py
@@ -1,59 +1,18 @@
 # -*- coding: iso-8859-1 -*-
 
-def add_options(parser):
-    parser.usage = 'lgp prepare [options] <project_dir> ...'
+import os, os.path as osp
+import sys
+from stat import ST_MTIME, S_IWUSR
+from cStringIO import StringIO
+from logilab.common.shellutils import cp
 
-def run(options, args):
-    pass
+from logilab.devtools import TEMPLATE_DIR
+from logilab.devtools.lib import TextReporter
+from logilab.devtools.lib.utils import cond_exec, confirm
+from logilab.devtools.lib.pkginfo import PackageInfo
+from logilab.devtools.lib.changelog import ChangeLog
 
-# XXX FIXME translate this
-"""
-#!/bin/bash
-#
-# prepare a distribution for a logilab python package after some checks
-#
-
-function cond_exit() {
-    # demande confirmation, quitte si non
-    echo -n "Continuer [y/N/r] ? "
-    read _a
-    if [ "$_a" = "y" ]; then
-	return 0
-    elif [ "$_a" = "r" ]; then
-	return 1
-    fi
-    exit 0
-}
-
-function cond_exec() {
-    # demande confirmation, retourne 0 si oui, 1 si non
-    echo -n "Executer $1 [Y/n] ? "
-    read _a
-    if [ "$_a" = "n" ]; then
-	return 1
-    fi
-    return 0
-}
-
-function install_copying() {
-    license=`python -c "from __pkginfo__ import license; import os; from logilab.devtools import templates; print os.path.join(os.path.dirname(templates.__file__), 'licenses', 'full_%s.txt'%license)"`
-    if [ $license -nt COPYING ] ; then
-        cp $license COPYING 
-        echo Copie du fichier $license dans COPYING
-    else
-        echo Fichier COPYING à jour
-    fi
-}
-
-function checkpackage_python() {
-    checkpackage
-}
-
-function checkpackage_formation() {
-    checkpackage -o release_number
-}
-
-ADDITIONAL_DESCR="LOGILAB provides services in the fields of XML techniques and advanced
+ADDITIONAL_DESCR="""LOGILAB provides services in the fields of XML techniques and advanced
 computing (implementation of intelligent agents, knowledge management,
 natural language processing, statistical analysis, data mining, etc.),
 and also trainings on Python, XML, UML, Object Oriented design, design
@@ -62,94 +21,135 @@ Logilab, visit http://www.logilab.com/.
 
 Logilab is also a strong supporter of the Free Software movement, and an
 active member of the Python and Debian communities. Logilab's open 
-source projects can be found on http://www.logilab.org/."
+source projects can be found on http://www.logilab.org/."""
 
-if [ "$1" = "" -o "$1" = "--help" -o "$1" = "-h" ]; then
-    echo "USAGE: preparedistrib <source_directory>"
-else
-    PKGNAME=`basename $1`
-    BASEDIR=$PWD
-    if [ -f $1/__init__.py ]; then
-	PKGTYPE="python"
-    else
-	PKGTYPE="formation"
-    fi
-    
-    if [ "$PKGTYPE" = "python" ]; then
-        echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        cond_exec pylint && while true; do pylint --ignore doc $PKGNAME && cond_exit && break; done 
-    fi
-    
-    pushd $1
-    if [ "$PKGTYPE" = "python" ]; then
-        echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        cond_exec "installation du fichier COPYING"  && install_copying
-    fi
-    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    if [ "$PKGTYPE" = "python" ]; then
-	cond_exec "vérification du paquetage" && while true; do checkpackage_python && cond_exit && break; done
-    fi
-    
-    if [ -d test ]
-	then
-	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	if cond_exec "lancement des tests"  ; then
-	    pushd test
-            while true; do
-	        python runtests.py
-                if [ $? -ne 0 ]; then
-                    echo "ERREUR: les tests ont échoués"
-                    cond_exit && break
-                else
-		    break
-		fi
-	    done
-	    popd
-	fi
-    fi
+SEPARATOR = '+' * 72
 
-    if [ -f ChangeLog ]; then
-	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	cond_exec "fermer le ChangeLog" && (
-	    chmod u+w ChangeLog
-	    changelog close
-#	    if [ $? -eq 0 ] ; then
-#		cvs ci -m "close change log for revision $PKGVER" ChangeLog
-#	    else
-#		cvs unedit ChangeLog
-#	    fi
-	)
-    fi
+def install_copying(license):
+    """ensure COPYING is up to date"""
+    license_file = osp.join(TEMPLATE_DIR, 'licenses', 'full_%s.txt' % license)
+    if osp.isfile(license_file) and confirm('COPYING file checking ?'):
+        if osp.isfile('COPYING'):
+            # update COPYING file if needed
+            if os.stat(license_file)[ST_MTIME] > os.stat('COPYING')[ST_MTIME]:
+                cp(license_file, 'COPYING')
+            else:
+                print "COPYING file is up to date"
+        else:
+            cp(license_file, 'COPYING')
 
-    if [ -d doc ]; then
-	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	if cond_exec "construction de la documentation" ; then
-	    pushd doc
-            while true; do
-                make
-                if [ $? -ne 0 ]; then
-                    echo "ERREUR: impossible de construire la documentation"
-                    cond_exit && break
-                else
-		    break
-		fi
-	    done
-	    popd
-	fi
-    fi
 
-    if [ "$PKGTYPE" = "python" ] && [ -f announce.txt ]; then
-	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	cond_exec "génération de l'annonce dans $BASEDIR/dist/$PKGNAME.announce" && (
-            CLOG=`changelog extract`
-            VARS=`python -c 'from __pkginfo__ import version, web, ftp, mailinglist ; print "VERSION %(version)s WEB %(web)s FTP %(ftp)s MAILINGLIST %(mailinglist)s"%globals()'`
-            LDESC=`pkginfo dump long_desc`
-            DISTNAME=`pkginfo dump name`
-	    stp announce.txt CHANGELOG "$CLOG" $VARS LONG_DESC "$LDESC" DISTNAME "$DISTNAME" ADDITIONAL_DESCR "$ADDITIONAL_DESCR" > $BASEDIR/dist/$PKGNAME.announce
-	)
-    fi
+def runtests(projdir=os.getcwd()):
+    """runs unit tests"""
+    if osp.isdir('test'):
+        os.chdir('test')
+        cond_exec('python runtests.py', confirm=True, retry=True)
+        os.chdir(projdir)
 
-    popd
+def close_changelog(projdir=os.getcwd()):
+    """closes the projects's ChangeLog"""
+    if osp.isfile('ChangeLog') and confirm('close ChangeLog ?'):
+        # chmod u+w ChangeLog
+        os.system('chmod 664 ChangeLog')
+        # os.system('changelog close')
+        chlg = ChangeLog('ChangeLog')
+        try:
+            chlg.close(projdir)
+        except Exception, exc:
+            print "An error occured while closing Changelog:", exc
+            if not confirm('Continue ?'):
+                sys.exit(1)
 
-fi
-"""
+
+def build_documentation(projdir=os.getcwd()):
+    """builds the project's documentation"""
+    if osp.isdir('doc') and (osp.isfile('doc/makefile') or osp.isfile('doc/Makefile')) and \
+           confirm('build documentation ?'):
+        os.chdir('doc')
+        cond_exec('make', retry=True)
+        os.chdir(projdir)
+
+
+def make_announce(pkginfo, filename):
+    if osp.isfile('announce.txt') and confirm('generate announe in %s ?' % filename):
+        stream = StringIO()
+        chglog = ChangeLog('ChangeLog')
+        chglog.extract(stream=stream)
+        whatsnew = stream.getvalue()
+        values = dict(CHANGELOG=whatsnew, VERSION=pkginfo.version,
+                      WEB=pkginfo.web, FTP=pkginfo.ftp,
+                      MAILINGLIST=pkginfo.mailinglist,
+                      LONG_DESC=pkginfo.long_desc, DISTNAME=pkginfo.name,
+                      ADDITIONAL_DESCR=ADDITIONAL_DESCR)
+        template = file('announce.txt').read()
+        announce = file(filename, 'w')
+        announce.write(template % values)
+        announce.close()
+
+
+DEFAULT_ACTIONS = ('pylint', 'copying', 'checkpackage',
+                   'runtests', 'changelog', 'doc', 'announce')
+
+def add_options(parser):
+    parser.usage = """lgp prepare [options] [<project_dir>]
+if <project_dir> is omitted, the current directory will be used
+
+possible actions are: %s
+""" % ', '.join(DEFAULT_ACTIONS)
+    parser.add_option('-o', '--only', action="append", dest="only",
+                      help="perform only that action (this option can be passed several times)",
+                      metavar='<ACTIONS>', default=[])
+    parser.max_args = 1
+
+
+def run(options, args):
+    projdir = osp.abspath(args and args[0] or os.getcwd())
+    distdir = osp.abspath(osp.join(os.getcwd(), '..', 'dist'))
+    os.chdir(projdir)
+    if osp.isfile('__init__.py'):
+        pkgtype = 'python'
+    else:
+        pkgtype = 'formation'
+    try:
+        pkginfo = PackageInfo(TextReporter(sys.stderr), projdir)
+    except ImportError, exc:
+        sys.stderr.write("%r does not appear to be a valid package " % projdir)
+        sys.stderr.write("(no __pkginfo__ found)\n")
+        return
+    actions = options.only or DEFAULT_ACTIONS
+    if pkgtype == 'python':
+        if 'pylint' in actions:
+            # run pylint
+            print SEPARATOR
+            cond_exec('pylint --ignore doc %s' % projdir, confirm=True, retry=True)
+        # update COPYING (license) file
+        if pkginfo.license and 'copying' in actions:
+            print SEPARATOR
+            install_copying(pkginfo.license)
+        # checkpackage
+        if 'checkpackage' in actions:
+            print SEPARATOR
+            cond_exec('lgp check', confirm=True, retry=True)
+    else:
+        # formation check only release number
+        if 'checkpackage' in actions:
+            print SEPARATOR
+            cond_exec('lgp check -o release_number', confirm=True, retry=True)
+    if 'runtests' in actions:
+        # run unit tests
+        print SEPARATOR
+        runtests()
+    if 'changelog' in actions:
+        # close project's ChangeLog
+        print SEPARATOR
+        close_changelog()
+    if 'doc' in actions:
+        # builds the documentation
+        print SEPARATOR
+        build_documentation()
+    if 'announce' in actions:
+        # prepare ANNOUNCE file
+        if pkgtype == 'python':
+            print SEPARATOR
+            filename = '%s/%s.announce' % (distdir, pkginfo.name)
+            make_announce(pkginfo, filename)
