273fdf056fdc40d3c3c5a703ddac3c3dc2789bd5
Sylvain <syt@logilab.fr>
2008-03-20 13:44:38 +0100

    move part of the changelog module to logilab.common

--- test/unittest_changelog.py
+++ test/unittest_changelog.py
@@ -1,12 +1,10 @@
-__revision__ = '$Id: unittest_changelog.py,v 1.3 2005-07-01 19:35:50 nico Exp $'
-
 import unittest
 from os.path import join, dirname
 from cStringIO import StringIO
 
 from logilab.common.testlib import TestCase
 
-from logilab.devtools.lib.changelog import *
+from logilab.devtools.lib.changelog import ChangeLog, DebianChangeLog
 
 class ChangeLogTC(TestCase):
     cl_class = ChangeLog
