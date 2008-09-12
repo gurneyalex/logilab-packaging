701ba93ada61ff95f34edbd6a0bae845966f053a
Julien Jehannet <julien.jehannet@logilab.fr>
2008-09-12 17:42:50 +0200

    minor: update docstring

--- changelog.py
+++ changelog.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-"""command line wrapper for lib.changelog"""
+"""command line wrapper for lgp.changelog"""
 
 import sys
 import getopt
