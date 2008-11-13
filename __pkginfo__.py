09fc0329b280d4c50fe67bec1fd510b0a84a5001
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2008-11-14 00:22:36 +0100

    0.12.2

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -20,7 +20,7 @@ logilab.devtools packaging information
 
 modname = 'devtools'
 distname = 'logilab-devtools'
-numversion = (0, 12, 1)
+numversion = (0, 12, 2)
 version = '.'.join([str(num) for num in numversion])
 
 license = 'GPL'
