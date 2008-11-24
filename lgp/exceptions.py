638ed1f87f216a0fe2d7d9775e44887b2f5f4d40
Julien Jehannet <julien.jehannet@logilab.fr>
2008-11-24 18:58:01 +0100

    add a new LGPCommandException for subprocesses

--- lgp/exceptions.py
+++ lgp/exceptions.py
@@ -6,9 +6,22 @@
 """
 __docformat__ = "restructuredtext en"
 
+import logging
+
+
 class LGPException(Exception):
     def __init__(self, value):
         self.value = value
+    def __str__(self):
+        return self.value
+
+class LGPCommandException(LGPException):
+    def __init__(self, value, cmd=None):
+        self.value = value
+        if cmd:
+            msg = "command '%s' returned non-zero exit status %s" \
+                  % (' '.join(cmd.cmd), cmd.returncode)
+            logging.error(msg)
     def __str__(self):
         return self.value
  
