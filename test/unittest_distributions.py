1c3eb670c1566b5d3520475be2516bfedffbe532
Julien Jehannet <julien.jehannet@logilab.fr>
2009-07-07 19:41:00 +0200

    get rid of distribution names hardcoded in code. Could certainly be improved later

--- test/unittest_distributions.py
+++ test/unittest_distributions.py
@@ -1,41 +1,51 @@
 import unittest
+import os.path
 
 from logilab.common.testlib import TestCase
-from logilab.devtools.lgp.utils import get_distributions, KNOWN_DISTRIBUTIONS
+from logilab.devtools.lgp.utils import get_distributions
 from logilab.devtools.lgp.exceptions import DistributionException
+
 
 
 class DistributionTC(TestCase):
 
-    def test_default_distribution(self):
-        # important for help generation
-        self.assertEquals(get_distributions("known"), tuple(set(KNOWN_DISTRIBUTIONS.keys())))
-        self.assertEquals(get_distributions(), tuple(set(KNOWN_DISTRIBUTIONS.keys())))
-        self.assertEquals(get_distributions("known"), tuple(set(KNOWN_DISTRIBUTIONS)))
-        self.assertEquals(get_distributions(), tuple(set(KNOWN_DISTRIBUTIONS)))
+    def setUp(self):
+        self.suites = os.path.abspath('data/suites')
+        self.basetgz = os.path.abspath('data/basetgz')
+        self.known = set(['hardy', 'intrepid', 'gutsy', 'lenny', 'oldstable',
+                         'stable', 'breezy', 'edgy', 'feisty', 'jaunty',
+                         'testing', 'sid', 'unstable', 'etch', 'squeeze',
+                         'dapper'])
 
-    def test_one_valid_distribution(self):
-        distrib = ['testing']
-        self.assertEquals(get_distributions(distrib), tuple(distrib))
+    def test_default_distribution(self):
+        self.assertUnorderedIterableEquals(get_distributions(suites=self.suites),
+                                           self.known)
+
+    def test_valid_distribution(self):
+        for distrib in ['stable', 'testing', 'sid', 'unstable']:
+            self.assertEquals(get_distributions([distrib,],
+                                                self.basetgz, self.suites),
+                              (distrib,))
 
     def test_several_valid_distributions(self):
-        distrib = ["unstable","testing"]
-        self.assertEquals(sorted(get_distributions(distrib)), sorted(distrib))
-
-    def test_one_unvalid_distribution(self):
-        distrib = ['winnie the pooh']
-        self.assertRaises(DistributionException, get_distributions, distrib)
-
-    def test_codename_distribution(self):
-        distrib = ['sid']
-        self.assertNotEquals(get_distributions(distrib), distrib)
-
-    def test_mixed_unvalid_distributions(self):
-        distrib = get_distributions() + ('winnie the pooh',)
-        self.assertRaises(DistributionException, get_distributions, distrib)
+        distrib = ['stable', 'testing', 'sid', 'unstable']
+        self.assertUnorderedIterableEquals(get_distributions(distrib,
+                                                             self.basetgz,
+                                                             self.suites),
+                                           distrib)
 
     def test_all_distribution_keyword(self):
-        pass
+        self.assertUnorderedIterableEquals(get_distributions("all", basetgz=self.basetgz),
+                                           set(["hardy", "intrepid", "jaunty", "lenny", "sid", "squeeze"]))
+        self.assertRaises(DistributionException, get_distributions, ['unknown'],
+                         basetgz=self.basetgz)
+
+    def test_one_unvalid_distribution(self):
+        distrib = ['unknown']
+        self.assertRaises(DistributionException, get_distributions, distrib, self.basetgz)
+        distrib = get_distributions(basetgz=self.basetgz, suites=self.suites) + \
+                  tuple(distrib,)
+        self.assertRaises(DistributionException, get_distributions, distrib, self.basetgz)
 
 if __name__  == '__main__':
     unittest.main()
