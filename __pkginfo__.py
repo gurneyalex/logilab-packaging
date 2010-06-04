90e15963b8e0d2ca1cb903ffeaafc053c7692b89
Julien Jehannet <julien.jehannet@logilab.fr>
2010-06-04 17:13:54 +0200

    debian: prepare 0.17.1

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -19,7 +19,7 @@ logilab.devtools packaging information
 
 modname = 'devtools'
 distname = 'logilab-devtools'
-numversion = (0, 17, 0)
+numversion = (0, 17, 1)
 version = '.'.join([str(num) for num in numversion])
 
 license = 'GPL'
