69d184f17b275f1d685c5372d657f5d7d2eb85c2
Julien Cristau <julien.cristau@logilab.fr>
2011-05-19 11:06:17 +0200

    Kill lgp piuparts (closes #66587)
    That command is not used

--- lgp/__init__.py
+++ lgp/__init__.py
@@ -45,8 +45,6 @@ def run(args):
                  'Log into a chrooted distribution'),
                 ('clean', 'logilab.devtools.lgp.clean',
                  'clean repository'),
-                ('piuparts', 'logilab.devtools.lgp.piuparts',
-                 'run piuparts with lgp configuration'),
                 ('project', 'logilab.devtools.lgp.project',
                  'print project information'),
                ]
@@ -54,7 +52,7 @@ def run(args):
     if len(sys.argv) <= 1:
         return parser.usage
     elif sys.argv[1] in ("build", "check", "clean", "template", "setup",
-                         "login", "tag", "project", "script", "piuparts"):
+                         "login", "tag", "project", "script"):
         exec 'from logilab.devtools.lgp.%s import run' % sys.argv[1]
         return run(args[1:])
     else:
