bbff9fdc10dfbb41d5f86e8929ecd7a4f01cc191
Julien Cristau <julien.cristau@logilab.fr>
2011-06-09 18:18:08 +0200

    Add test for make_debian_source_package

--- test/unittest_build.py
+++ test/unittest_build.py
@@ -19,11 +19,15 @@ class BuildTC(TestCase):
     def test_make_tarball_rev1(self):
         os.chdir(os.path.join(os.path.dirname(__file__), 'data/packages/first'))
         builder = build.Builder()
+        builder.go_into_package_dir(None)
         builder._set_package_format()
 
         with tempdir(False) as tmpdir:
             tgz = builder.make_orig_tarball(tmpdir)
             self.assertTrue(os.path.exists(tgz))
+            with tempdir(False) as tmpdir2:
+                builder.make_debian_source_package('sid', tmpdir=tmpdir2)
+                self.assertTrue(os.path.exists(os.path.join(tmpdir2, 'test_1-1.dsc')))
 
     def test_make_tarball_rev2(self):
         os.chdir(os.path.join(os.path.dirname(__file__), 'data/packages/next'))
